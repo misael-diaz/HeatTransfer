b88a23c65d81f59a666866fdea828a5ee6da87d3
misael-diaz <misael.diaz@upr.edu>
2021-09-29 13:26:50 -0400

    limits the source to 75 characters

--- introduction/heat-transfer-modes/convection/examples/p-1.19-power-transistor-convection-heat-transfer.py
+++ introduction/heat-transfer-modes/convection/examples/p-1.19-power-transistor-convection-heat-transfer.py
@@ -1,13 +1,14 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Heat Transfer I                                                 March 23, 2020
+Heat Transfer I                                              March 23, 2020
 ME 3150 SP20  
 Prof. M. Diaz-Maldonado
 
 Synopsis:
 Problem 1.27. Heat removal from a power transistor by forced convection.
 
-Incroprera et al. Fundamentals of Heat and Mass Transfer, 7th edition. 
+Incroprera et al. Fundamentals of Heat and Mass Transfer, 7th edition.
 Physical quantities are expressed in SI Units.
 """
 
@@ -21,7 +22,7 @@ surface area,     meters squared, m^2
 """
 L = 10.0e-3
 D = 12.0e-3
-A = pi * D * L 
+A = pi * D * L
 
 """
 heat transfer coefficient,             W / m^2 K
@@ -36,7 +37,7 @@ T_inf = 25.0
 solution (part a):
 generated thermal energy, Watts,         W
 """
-Eg = h * A * (T_s - T_inf) 
+Eg = h * A * (T_s - T_inf)
 
 print("")
 print("Answers:")
