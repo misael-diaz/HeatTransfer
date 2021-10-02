55f5e81e526eebbcde4dbda282a93f6bf563e6f9
misael-diaz <misael.diaz@upr.edu>
2021-10-02 12:00:51 -0400

    applies vertical alignment (1.18)
    removes trailing whitespace

--- introduction/heat-transfer-modes/convection/examples/p-1.18-isothermal-chip-convection-heat-transfer.py
+++ introduction/heat-transfer-modes/convection/examples/p-1.18-isothermal-chip-convection-heat-transfer.py
@@ -25,38 +25,38 @@ References:
 """
 
 """
-length,   meters,         m
-surface area,     meters squared, m^2
+length,         meters,         m
+surface area,   meters squared, m^2
 """
 W = 5.0e-3
 A = W * W
 
 """
-heat transfer coefficient,             W / m^2 K
-surface temperature,      Centigrades, C
-ambient temperature,      Centigrades, C
+heat transfer coefficient,                  W / m^2 K
+surface temperature,        Centigrades,    C
+ambient temperature,        Centigrades,    C
 """
 h     = 200.0
 T_s   = 85.0
 T_inf = 15.0
 
-""" 
-solution (part a):
-generated thermal energy, Watts,         W
 """
-Eg = h * A * (T_s - T_inf) 
+solution (part a):
+generated thermal energy,   Watts,  W
+"""
+Eg = h * A * (T_s - T_inf)
 
 print("")
 print("Answers:")
 print("max allowed chip power with air as coolant: %e W" %(Eg))
-""" 
+"""
 convection heat transfer coefficient for the dielectric fluid, W / m^2 K
 """
 h = 3000.0
-""" 
-solution (part b):
-generated thermal energy, Watts,         W
 """
-Eg = h * A * (T_s - T_inf) 
+solution (part b):
+generated thermal energy,   Watts,  W
+"""
+Eg = h * A * (T_s - T_inf)
 print("max allowed chip power with the dielectric fluid as coolant: %e W" %(Eg))
 print("")
